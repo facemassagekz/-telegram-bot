2f4395e2e0f46d470d30a28be73c18dbc4620ef7
facemassagekz <anjelaa266@gmail.com>
2026-04-26 18:10:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,14 @@
-from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8002113464:AAHjClyZmVu6WOmn-sx-t4pDBCaLilWf-9k"
-
-menu = ReplyKeyboardMarkup([
-    ["💳 Оплата"],
-    ["🎥 Курсы"],
-    ["🔁 Возврат"],
-    ["💆‍♀️ Массаж"],
-    ["📩 Поддержка"]
-], resize_keyboard=True)
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет 👋 Выберите раздел:", reply_markup=menu)
-
-async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Вы выбрали: " + update.message.text)
+    await update.message.reply_text("Бот работает ✔")
 
 app = Application.builder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
