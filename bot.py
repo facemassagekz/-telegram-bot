636dc96bab735b0c05563410d0202264f4f50176
facemassagekz <anjelaa266@gmail.com>
2026-04-26 23:29:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает ✔")
 
-app = Application.builder().token(TOKEN).build()
+app = app = Application.builder().token(TOKEN).build()
+app.run_polling(drop_pending_updates=True)
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
